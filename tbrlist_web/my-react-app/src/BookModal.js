6a34f3f4cb7815f2d12478bc917fb241c31f344b
niha-raj <143101444+niha-raj@users.noreply.github.com>
2025-05-04 17:26:05 -0400

    added stuff and then removed it anyways

--- tbrlist_web/my-react-app/src/BookModal.js
+++ tbrlist_web/my-react-app/src/BookModal.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 // AddBookModal component
 function AddBookModal({ isOpen, onClose, bookData, onInputChange, onSubmit }) {
   if (!isOpen) return null;
